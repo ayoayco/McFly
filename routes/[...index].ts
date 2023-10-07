77a93f85074668b0f7d7167bb0712f2ab7fe5b30
Ayo <ramon.aycojr@gmail.com>
2023-10-07 22:45:45 +0200

    feat: nested sub pages

--- routes/[...index].ts
+++ routes/[...index].ts
@@ -1,7 +1,22 @@
 import * as fs from "fs";
 export default eventHandler((event) => {
   const filename = event.path === "/" ? "/index.html" : `${event.path}.html`;
-  const path = `./src/pages${filename}`;
-  const html = fs.readFileSync(path, "utf8");
+  const fallback = getPath(event.path + "/index.html");
+  const path = getPath(filename);
+  let html = "";
+  try {
+    html = fs.readFileSync(path, "utf8");
+  } catch (error) {
+    try {
+      html = fs.readFileSync(fallback, "utf8");
+    } catch {
+      html = `cannot find ${path} or ${fallback}`;
+    }
+  }
+
   return html;
 });
+
+function getPath(filename: string) {
+  return `./pages${filename}`;
+}
