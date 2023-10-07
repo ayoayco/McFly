88529ca8d1af2ca6e7eb3ba1a40f382a58879756
Ayo <ramon.aycojr@gmail.com>
2023-10-07 21:59:10 +0200

    feat: registry object

--- public/registry.js
+++ public/registry.js
@@ -1,2 +1,13 @@
 import HelloWorld from "/components/hello-world.js";
-customElements.define("hello-world", HelloWorld);
+
+/**
+ * Map of custom element name to the class constructor
+ */
+const registry = {
+  "hello-world": HelloWorld,
+};
+
+Object.keys(registry).forEach((key) => {
+  if (window?.hasOwnProperty("customElements"))
+    customElements.define(key, registry[key]);
+});
