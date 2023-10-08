ef3377ec7d335fc57204ea825c39a6d87bbc570b
Ayo <ramon.aycojr@gmail.com>
2023-10-08 09:20:38 +0200

    test output directory

---
+++ plugins/1-copy-components.ts
@@ -0,0 +1,11 @@
+import * as fs from "fs";
+export default defineNitroPlugin(async () => {
+  console.log("Copying components to public folder...");
+  const rawKeys = await useStorage().getKeys("/assets/components");
+  rawKeys.forEach(async (key) => {
+    const cleanKey = key.replace("assets:components:", "");
+    const content = await useStorage().getItem(key);
+    if (!fs.existsSync("./public/output")) fs.mkdirSync("./public/output");
+    fs.writeFileSync(`./public/output/${cleanKey}`, content.toString());
+  });
+});
