03bfab016379121fb16f6a8185639275c9c797f6
Ayo <ramon.aycojr@gmail.com>
2023-10-10 18:48:41 +0200

    try node

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  preset: "vercel-edge",
+  preset: "node",
   devServer: {
     watch: ["./src/pages", "./src/components"],
   },
