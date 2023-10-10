bf209c1db0b15b485fb42aa3ee0a2f5455a3040e
Ayo <ramon.aycojr@gmail.com>
2023-10-10 18:46:19 +0200

    try vercel-static

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  preset: "vercel",
+  preset: "vercel-static",
   devServer: {
     watch: ["./src/pages", "./src/components"],
   },
