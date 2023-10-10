34065ad0c5a08d361b62d253cff814c0fd5312a5
Ayo <ramon.aycojr@gmail.com>
2023-10-10 18:32:06 +0200

    refactor: use safe storage keys

--- plugins/1-copy-components.ts
+++ plugins/1-copy-components.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 export default defineNitroPlugin(async () => {
   console.log("Copying components to public folder...");
-  const rawKeys = await useStorage().getKeys("/assets/components");
+  const rawKeys = await useStorage().getKeys("assets:components");
   rawKeys.forEach(async (key) => {
     const cleanKey = key.replace("assets:components:", "");
     const content = await useStorage().getItem(key);
