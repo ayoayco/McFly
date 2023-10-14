058b1127474bdda4da6b5211fa6ea13184077176
Ayo <ramon.aycojr@gmail.com>
2023-10-14 22:26:50 +0200

    test: absolute minimum complexity

--- mcfly.config.ts
+++ mcfly.config.ts
@@ -1,6 +1,6 @@
 import defineConfig from "./packages/define-config";
-import registerComponents from "./packages/register-components";
+// import registerComponents from "./packages/register-components";
 
 export default defineConfig({
-  onBuild: [registerComponents()],
+  // onBuild: [registerComponents()],
 });
--- packages/define-config.ts
+++ packages/define-config.ts
@@ -1,10 +1,8 @@
 import { NitroApp } from "nitropack";
-import setUpSsr from "./set-up-ssr";
 
 export type McFlyConfig = {
   onBuild?: Array<(event: NitroApp) => void>;
 };
 export default function defineConfig(config: McFlyConfig) {
-  const onBuild = config.onBuild ?? [];
-  return () => ({ ...config, onBuild: [...onBuild, setUpSsr()] });
+  return () => config;
 }
