9b6043b219ab4d8c24f3f5f3c998017bdcbc2e3e
Ayo <ramon.aycojr@gmail.com>
2023-10-20 11:23:57 +0200

    refactor: examples to use `data-name` prop

--- src/components/my-hello-world.js
+++ src/components/my-hello-world.js
@@ -1,15 +1,15 @@
 class HelloWorld extends WebComponent {
-  static properties = ["name"];
+  static properties = ["data-name"];
 
   onInit() {
     let count = 0;
     this.onclick = () => {
-      this.setAttribute("name", `Clicked ${++count}x`);
+      this.setAttribute("data-name", `Clicked ${++count}x`);
     };
     this.setAttribute("title", "Click me please");
   }
 
   get template() {
-    return `<button style="cursor:pointer">Hello ${this.name}!</button>`;
+    return `<button style="cursor:pointer">Hello ${this["data-name"]}!</button>`;
   }
 }
--- src/components/vanilla-hello-world.js
+++ src/components/vanilla-hello-world.js
@@ -1,17 +1,17 @@
 class HelloWorld extends HTMLElement {
   static get observedAttributes() {
-    return ["name"];
+    return ["data-name"];
   }
 
   connectedCallback() {
     let count = 0;
     this.onclick = () => {
-      this.setAttribute("name", `Clicked ${++count}x`);
+      this.setAttribute("data-name", `Clicked ${++count}x`);
     };
   }
 
   attributeChangedCallback(property, previousValue, currentValue) {
-    if (previousValue !== currentValue) {
+    if (property === "data-name" && previousValue !== currentValue) {
       this.innerHTML = `<button style="cursor:pointer">Hello ${currentValue}!</button>`;
     }
   }
