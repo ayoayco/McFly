5603e3a5eedec5848228f0457db92f77e23c9ffa
Ayo <ramon.aycojr@gmail.com>
2023-10-21 08:24:15 +0200

    feat(create-mcfly): initial script

---
+++ packages/create-mcfly/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// const fs = require('fs');
+// const args = process.argv;
+
+create();
+
+function create() {
+  console.log("Create a new McFly app!");
+  console.log(
+    "This is not ready yet, see steps on how to create a new McFly app in our project README: https://ayco.io/gh/McFly#readme"
+  );
+  return 1;
+}
