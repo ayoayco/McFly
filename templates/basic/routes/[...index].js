465cde6ea11744f71a337708a2a1edb18d8f6fd2
Ayo <ramon.aycojr@gmail.com>
2023-10-21 17:58:03 +0200

    refactor: all .ts to .js files 😱

--- templates/basic/routes/[...index].js
+++ templates/basic/routes/[...index].js
@@ -2,5 +2,5 @@
  * McFly SSR logic
  */
 import { defineRoute } from "@mcflyjs/core/event-handler.mjs";
-import config from "../mcfly.config";
+import config from "../mcfly.config.mjs";
 export default defineRoute({ config, storage: useStorage() });
