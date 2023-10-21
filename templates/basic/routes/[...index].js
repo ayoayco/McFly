ca19fe2ab999a2e837aaf0947d4fb5e16b3eb9f7
Ayo <ramon.aycojr@gmail.com>
2023-10-22 00:56:53 +0200

    chore(site,templates): add more info in routes

--- templates/basic/routes/[...index].js
+++ templates/basic/routes/[...index].js
@@ -1,5 +1,9 @@
 /**
  * McFly SSR logic
+ * 👋 this is not the route you're looking for
+ * ...pages are in ./src/pages
+ * ...components/fragments are in ./src/components
+ * @see more on https://ayco.io/gh/McFly
  */
 import { defineRoute } from "@mcflyjs/core/event-handler.mjs";
 import config from "../mcfly.config.mjs";
