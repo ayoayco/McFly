de89c9df5a7396d811fd0d487673cdc396492446
Ayo <ramon.aycojr@gmail.com>
2023-10-21 21:13:35 +0200

    feat(create-mcfly): catch error

--- packages/create-mcfly/index.js
+++ packages/create-mcfly/index.js
@@ -3,12 +3,16 @@
 const { downloadTemplate } = require("giget");
 create();
 async function create() {
-  const { source, dir } = await downloadTemplate(
-    "github:ayoayco/mcfly/templates/basic",
-    {
-      dir: "mcfly-app",
-    }
-  );
-  console.log(`✨ New McFly app created: ${dir}`);
+  try {
+    const { source, dir } = await downloadTemplate(
+      "github:ayoayco/mcfly/templates/basic",
+      {
+        dir: "mcfly-app",
+      }
+    );
+    console.log(`✨ New McFly app created: ${dir}`);
+  } catch (e) {
+    console.error('😱 "mcfly-app" directory already exists');
+  }
   return 1;
 }
