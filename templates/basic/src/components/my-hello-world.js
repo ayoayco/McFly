fa4711031ce7c85ca642e1f003ea626a654e0d2b
Ayo <ramon.aycojr@gmail.com>
2023-10-21 13:47:02 +0200

    feat(templates): add content to basic template

---
+++ templates/basic/src/components/my-hello-world.js
@@ -0,0 +1,19 @@
+/**
+ * Custom element using a minimal Web Component Base class
+ * @see https://ayco.io/n/web-component-base
+ */
+class HelloWorld extends WebComponent {
+  static properties = ["data-name"];
+
+  onInit() {
+    let count = 0;
+    this.onclick = () => {
+      this.setAttribute("data-name", `Clicked ${++count}x`);
+    };
+    this.setAttribute("title", "Click me please");
+  }
+
+  get template() {
+    return `<button style="cursor:pointer">Hello ${this.dataName}!</button>`;
+  }
+}
