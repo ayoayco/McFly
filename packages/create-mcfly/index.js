5095f504e70cf04f1d685e0e0f2fa52b5ecd4e98
Ayo <ramon.aycojr@gmail.com>
2023-10-22 17:32:36 +0200

    feat(create-mcfly): install deps & init git

--- packages/create-mcfly/index.js
+++ packages/create-mcfly/index.js
@@ -1,18 +1,58 @@
 #!/usr/bin/env node
 
 const { downloadTemplate } = require("giget");
+const { consola } = require("consola");
+const { exec } = require("node:child_process");
 create();
 async function create() {
+  console.clear();
+  let hasErrors = false;
+  const directory =
+    (await consola.prompt("Name your new vanilla web app:", {
+      placeholder: "./mcfly-app",
+    })) ?? "mcfly-app";
   try {
-    const { source, dir } = await downloadTemplate(
-      "github:ayoayco/mcfly/templates/basic",
+    await consola.start(`Copying template to ${directory}...`);
+    await downloadTemplate("github:ayoayco/mcfly/templates/basic", {
+      dir: directory,
+    });
+  } catch (e) {
+    consola.error(e);
+    hasErrors = true;
+  }
+
+  if (!hasErrors) {
+    const installDeps = await consola.prompt(
+      "Would you like to install the dependencies?",
       {
-        dir: "mcfly-app",
+        type: "confirm",
       }
     );
-    console.log(`✨ New McFly app created: ${dir}`);
-  } catch (e) {
-    console.error('😱 "mcfly-app" directory already exists');
+    if (installDeps) {
+      await consola.start("Installing dependencies...");
+      try {
+        await exec(`npm --prefix ${directory} install`);
+      } catch (e) {
+        consola.error(e);
+      }
+    }
+
+    const initializeGit = await consola.prompt(
+      "Would you like to initialize your git repository?",
+      {
+        type: "confirm",
+      }
+    );
+    if (initializeGit) {
+      await consola.start("Initializing git repository...");
+      try {
+        await exec(`git -C ${directory} init`);
+      } catch (e) {
+        consola.error(e);
+      }
+    }
+
+    consola.box(`McFly app created: ${directory}`);
   }
   return 1;
 }
