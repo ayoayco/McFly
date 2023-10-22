15be588e0dd70a4ee979ae108ea9d5709fa6b917
Ayo <ramon.aycojr@gmail.com>
2023-10-22 21:07:03 +0200

    feat(site,templates): update route info

--- templates/basic/routes/[...index].js
+++ templates/basic/routes/[...index].js
@@ -2,8 +2,8 @@
  * McFly SSR logic
  * 👋 this is not the route you're looking for
  * ...pages are in ./src/pages
- * ...components/fragments are in ./src/components
- * @see more on https://ayco.io/gh/McFly
+ * ...reusable code are in ./src/components
+ * @see https://ayco.io/gh/McFly#special-directories
  */
 import { defineRoute } from "@mcflyjs/core/event-handler.mjs";
 import config from "../mcfly.config.mjs";
