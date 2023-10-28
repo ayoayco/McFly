60fc0ee053a16f9f1fe1ad79463e7eede13d05e1
Ayo <ramon.aycojr@gmail.com>
2023-10-28 23:29:41 +0200

    fix(templates/basic): route function handler

--- templates/basic/routes/[...index].js
+++ templates/basic/routes/[...index].js
@@ -5,6 +5,6 @@
  * ...reusable code are in ./src/components
  * @see https://ayco.io/gh/McFly#special-directories
  */
-import { defineRoute } from "@mcflyjs/core/event-handler.mjs";
+import { useMcFlyRoute } from "@mcflyjs/core/event-handler.mjs";
 import config from "../mcfly.config.mjs";
-export default defineRoute({ config, storage: useStorage() });
+export default useMcFlyRoute({ config, storage: useStorage() });
