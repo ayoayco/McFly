2e4d3f6598248793765cdc2370035645b4223dc8
Ayo <ramon.aycojr@gmail.com>
2023-10-31 09:53:41 +0100

    refactor(core): explicit exports

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,2 +1,2 @@
-export * from "./define-config.js";
-export * from "./event-handler.js";
+export { defineMcFlyConfig } from "./define-config.js";
+export { useMcFlyRoute } from "./event-handler.js";
