52b0c69db759aab88a4527afc7f87d71d4a8c123
Ayo <ramon.aycojr@gmail.com>
2023-11-03 22:31:29 +0100

    feat(cli): serve to show mcfly & nitro version

--- packages/cli/commands/serve.mjs
+++ packages/cli/commands/serve.mjs
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 import { consola } from "consola";
+import { colorize } from "consola/utils";
 import { defineCommand } from "citty";
-import { execSync as exec } from "child_process";
+import { execSync as exec } from "node:child_process";
+import { createRequire } from "node:module";
 
 export default defineCommand({
   meta: {
@@ -10,6 +12,22 @@ export default defineCommand({
     description: "Runs the dev server.",
   },
   async run() {
+    try {
+      const _require = createRequire(import.meta.url);
+      const mcflyPkg = await _require("@mcflyjs/core/package.json");
+      const mcflyPkgVersion = `McFly ${mcflyPkg.version}`;
+      const nitroPkg = await _require("nitropack/package.json");
+      const nitroPkgVersion = `Nitro ${nitroPkg.version}`;
+      consola.log(
+        `${colorize("blue", mcflyPkgVersion)} ${colorize(
+          "dim",
+          nitroPkgVersion
+        )}`
+      );
+    } catch (e) {
+      consola.error(e);
+    }
+
     try {
       exec(`npx nitropack dev`, { stdio: "inherit" });
     } catch (e) {
