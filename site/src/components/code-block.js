cb503f7ca3989196c70e4b60db5480f518431f57
Ayo <ramon.aycojr@gmail.com>
2023-11-12 16:42:23 +0100

    refactor(core): simpler code-block element

--- site/src/components/code-block.js
+++ site/src/components/code-block.js
@@ -2,18 +2,17 @@ class CodeBlockComponent extends HTMLElement {
   connectedCallback() {
     this.trimmed = this.innerHTML.trim();
     const lang = this.getAttribute("language");
-    const lineNumbers = this.getAttribute("line-numbers") === "true";
-
-    console.log(lineNumbers, lang);
 
     this.innerHTML = `
-      <div>
-        <pre class="language-${lang} ${
-      lineNumbers ? "line-numbers" : ""
-    }" id="pre" style="padding:1em;background:#efefef;margin:1em 0;border-radius:5px;font-size:large;"><code id="code">${
+        <pre id="pre" style="padding:1em;background:#efefef;margin:1em 0;border-radius:5px;font-size:large;"><code id="code">${
       this.trimmed
     }</code></pre>
-      </div>
     `;
+
+    if (lang) {
+      const pre = this.querySelector('#pre')
+      pre.className = `language-${lang}`;
+    }
+
   }
 }
