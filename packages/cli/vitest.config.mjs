025587c0e6799aabdab29a90a2a8513259d849d1
Ayo Ayco <ayo@ayco.io>
2024-12-07 18:35:14 +0100

    test(cli): test output default

--- packages/cli/vitest.config.mjs
+++ packages/cli/vitest.config.mjs
@@ -1,14 +1,12 @@
-import {coverageConfigDefaults, defineConfig} from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
-    reporters: ['html'],
     coverage: {
       enabled: true,
-      provider: 'v8',
-      reporter: ['html', 'text'],
-      exclude: ['html/**', ...coverageConfigDefaults.exclude]
-    }
+      provider: "v8",
+      reporter: ["html", "text"],
+      exclude: ["html/**", ...coverageConfigDefaults.exclude],
+    },
   },
-})
+});
