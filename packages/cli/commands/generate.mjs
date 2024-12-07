ea06089c4aa3c104680b223b116d014d718d4b1e
Ayo Ayco <ayo@ayco.io>
2024-12-07 20:40:15 +0100

    test(cli): add tests for generate command

--- packages/cli/commands/generate.mjs
+++ packages/cli/commands/generate.mjs
@@ -3,12 +3,20 @@
 import { consola } from "consola";
 import { defineCommand } from "citty";
 
+function generate() {
+  consola.box("Generate a McFly building block (In-progress)");
+}
+
 export default defineCommand({
   meta: {
     name: "prepare",
     description: "Generates building blocks for a McFly app.",
   },
-  async run() {
-    consola.box("Generate a McFly building block (In-progress)");
+  run() {
+    generate();
   },
 });
+
+export const exportedForTest = {
+  generate,
+};
---
+++ packages/cli/test/generate.test.js
@@ -0,0 +1,15 @@
+import { expect, test, vi } from "vitest";
+import { exportedForTest } from "../commands/generate.mjs";
+import consola from "consola";
+
+const testFn = exportedForTest.generate;
+
+test("show box message in-progress", () => {
+  const spy = vi.spyOn(consola, "box");
+
+  testFn();
+  const arg = spy.mock.calls[0][0];
+
+  expect(spy).toHaveBeenCalled();
+  expect(arg).toContain("In-progress");
+});
