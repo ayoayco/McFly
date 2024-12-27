7d1de4481f49099dc3eac6365a2eb2462982da00
Ayo Ayco <ayo@ayco.io>
2024-12-27 14:44:28 +0100

    chore: use .gitignore patterns in eslint ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  includeIgnoreFile(gitignorePath),
   {
-    ignores: [
-      'dist/*',
-      '.output/*',
-      '.nitro/*',
-      'site/*',
-      'templates/*',
-      '**/coverage/*',
-      '**/node_modules/*',
-    ],
+    ignores: ['site/*', 'templates/*'],
   },
 ]
