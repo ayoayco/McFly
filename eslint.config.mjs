03f7119f65703c89642e0ecc6a3ed2f795d2e215
Ayo Ayco <ayo@ayco.io>
2024-12-27 21:48:23 +0100

    chore: test precommit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
+const unused = 'hey'
+
+console.log(unused)
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
