3e1db19043963fb3bb86506e675fc5a80759e91b
Ayo Ayco <ayo@ayco.io>
2024-12-30 22:16:52 +0100

    feat: expose types for NitroConfig & McFlyConfig

--- packages/config/index.js
+++ packages/config/index.js
@@ -2,6 +2,11 @@ export { defineMcFlyConfig } from './define-mcfly-config.js'
 import { nitroConfig } from './nitro-config.js'
 
 /**
+ * @typedef {import('nitropack').NitroConfig} NitroConfig
+ * @typedef {import('./define-mcfly-config.js').McFlyConfig} McFlyConfig
+ */
+
+/**
  * Returns the Nitro configuration for a McFly project
  * @returns {NitroConfig}
  */
