8d976b8b471a133721035b9cfcd3a200550fe351
Ayo Ayco <ayo@ayco.io>
2024-12-31 00:40:42 +0100

    fix(template): import useMcFlyRoute

--- templates/basic/routes/[...].js
+++ templates/basic/routes/[...].js
@@ -5,5 +5,6 @@
  * ...reusable code are in ./src/components
  * @see https://ayco.io/gh/McFly#special-directories
  */
-import config from '../mcfly.config.mjs'
-export default useMcFlyRoute({ config, storage: useStorage() })
+
+import { useMcFlyRoute } from '@mcflyjs/core'
+export default useMcFlyRoute({ storage: useStorage() })
