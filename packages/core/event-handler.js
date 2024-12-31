2638b28533393ad7ca526344801c0e9146fdb408
Ayo Ayco <ayo@ayco.io>
2024-12-31 02:12:41 +0100

    feat: show logs for netlify debug

--- packages/core/event-handler.js
+++ packages/core/event-handler.js
@@ -35,9 +35,7 @@ export function useMcFlyRoute({ storage }) {
     const { components: componentType } = config
     let html = await getHtml(path, storage)
 
-    if (config?.server?.logs) {
-      consola.info('[INFO]: Config found\n', config)
-    }
+    consola.info('[INFO]: Config found\n', config)
 
     if (html) {
       const transforms = [evaluateServerScript, deleteServerScripts]
