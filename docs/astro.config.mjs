f2f35efe09cec2b9ed71b4642beace6389004ab1
Ayo Ayco <ayo@ayco.io>
2025-03-23 19:07:52 +0100

    feat: initial astro starlight setup

---
+++ docs/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'McFly',
+      social: {
+        github: 'https://github.com/ayoayco/mcfly',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+})
